bcf8cb1c1e1e74456e53c6cf27b491e0cf36691a
Polariche <Polariche@gmail.com>
2017-08-31 17:33:59 +0900

    Add comment to PlayerMove.cs

--- Assets/script/PlayerMove.cs
+++ Assets/script/PlayerMove.cs
@@ -9,6 +9,7 @@ public class PlayerMove : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        //이동키에 방향 배정
         Dictionary<KeyCode, Vector3> dict = new Dictionary<KeyCode, Vector3>(4);
         dict.Add(KeyCode.A, Vector3.left);
         dict.Add(KeyCode.D, Vector3.right);
@@ -17,26 +18,31 @@ public class PlayerMove : MonoBehaviour {
 
         foreach (KeyValuePair<KeyCode, Vector3> entry in dict)
         {
+            //키가 눌러졌고, 방향이 플레이어 기준 좌측 혹은 우측일 때
             if (Input.GetKey(entry.Key) && transform.up != entry.Value && transform.up != -entry.Value)
             {
                 RaycastHit hit;
                 if (Physics.Raycast(transform.position, entry.Value, out hit, StickRange))
                 {
+                    //가려는 방향에 벽이 있으면 그 벽에 붙음
                     StickToWall(hit);
                 }
                 else
                 {
+                    //이동
                     transform.position = transform.position + entry.Value * Speed;
                 }
             }
         }
 
+        //플레이어의 발쪽으로 레이캐스트
         RaycastHit groundHit;
         if (Physics.Raycast(transform.position, -transform.up, out groundHit, StickRange))
         {
             StickToWall(groundHit);
         } else
         {
+            //바닥이 없으면 우측, 좌측으로 레이캐스트 하고 거리가 짧은 쪽 벽에 붙음
             RaycastHit rightHit;
             RaycastHit leftHit;
 
@@ -56,6 +62,7 @@ public class PlayerMove : MonoBehaviour {
 
     void StickToWall(RaycastHit hit)
     {
+        //레이캐스트 정보를 바탕으로 벽에 붙음
         if (hit.collider != null) {
             Vector3 normal = hit.normal;
             Vector3 o = new Vector3(transform.position.x, transform.position.y, transform.position.z);
